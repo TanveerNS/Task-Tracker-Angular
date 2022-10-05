858717d9b08a17e6470d9ae3d040dfd71e616058
tanveerns <tanveeralam229@gmail.com>
2022-10-05 19:40:02 +0530

    json-server http

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -12,11 +12,24 @@ export class TaskItemComponent implements OnInit {
   task!: Task;
   faTimes = faTimes;
 
+  @Output() onDeleteTask:EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder:EventEmitter<Task> = new EventEmitter()
+
   iconColor:string = 'red'
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: any){
+    this.onDeleteTask.emit(task)
+    console.log('delete btn', task)
+  }
+
+  onToggle(task:Task){
+    this.onToggleReminder.emit(task)
+    console.log('toggle - ->',task)
   }
 
 }
